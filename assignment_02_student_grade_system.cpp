354c9871bd230be549acc422d15660ae01189d2c
niella300 <daadams003@st.ug.edu.gh>
2026-08-01 15:37:33 +0000

    Assignment 2 completed

--- assignment_02_student_grade_system.cpp
+++ assignment_02_student_grade_system.cpp
@@ -46,3 +46,36 @@
 #include <iostream>
 using namespace std;
 
+
+char getGrade(double score);
+
+int main() {
+    double score;
+
+    cout << "Enter student score (0-100); ";
+    cin >> score;
+
+    if (score < 0|| score > 100) {
+
+        cout << "Error: Score must be betweeen 0 and 100." << endl;
+        return 1; // non-zero return = program ended due to invalid input
+    }
+    char grade = getGrade(score);
+
+    return 0;
+}
+
+
+char getGrade(double score) {
+    if (score >= 80) {
+        return 'A';
+    } else if (score >= 70) {
+        return 'B';
+    } else if (score >= 60) {
+        return 'C';
+    } else if (score >= 50) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
